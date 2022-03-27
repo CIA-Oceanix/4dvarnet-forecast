d5a2856f10957a1ba6539b777dd4b3da68d6a154
rfablet <ronan.fablet@imt-atlantique.fr>
2022-03-27 23:41:33 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -885,8 +885,8 @@ if __name__ == '__main__':
             print("\n")
             print('..... Forecasting performance (all):')
             for nn in range(0,dt_forecast):
-                var_test_nn     = np.mean( (X_test[:,:,:dT+nn] - X_test[:,:,:dT])**2 )
-                mse_forecast = np.mean( (mod.x_rec[:,:,:dT+nn]-X_test[:,:,:dT+nn]) **2 ) 
+                var_test_nn     = np.mean( (X_test[:,:,dT+nn] - X_test[:,:,dT])**2 )
+                mse_forecast = np.mean( (mod.x_rec[:,:,dT+nn]-X_test[:,:,dT+nn]) **2 ) 
             
                 print('... dt [ %03d ] = %.3f / %.3f %.3f '%(nn,mse_forecast,mse_forecast/var_test,mse_forecast/var_test_nn) )                
             
@@ -894,8 +894,8 @@ if __name__ == '__main__':
             print('..... Forecasting performance (x1):')
             var_test_x1  = np.mean( (X_test[:,0,:] - np.mean(X_test[:,0,:],axis=0))**2 )
             for nn in range(0,dt_forecast):
-                var_test_nn     = np.mean( (X_test[:,0,:dT+nn] - X_test[:,0,:dT])**2 )
-                mse_forecast = np.mean( (mod.x_rec[:,0,:dT+nn]-X_test[:,0,:dT+nn]) **2 ) 
+                var_test_nn     = np.mean( (X_test[:,0,dT+nn] - X_test[:,0,dT])**2 )
+                mse_forecast = np.mean( (mod.x_rec[:,0,dT+nn]-X_test[:,0,:+nn]) **2 ) 
             
                 print('... dt [ %03d ] = %.3f / %.3f %.3f '%(nn,mse_forecast,mse_forecast/var_test,mse_forecast/var_test_nn) )                
         else:
