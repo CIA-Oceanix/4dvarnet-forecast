9a04aa0a5846e443355f471bd3b471099f340076
rfablet <ronan.fablet@imt-atlantique.fr>
2022-03-29 16:38:36 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1191,6 +1191,8 @@ if __name__ == '__main__':
         mod = LitModel.load_from_checkpoint(pathCheckPOint)            
         
         print(mod.hparams)
+        mod.hparams.alpha_mse_rec = 0.75
+        mod.hparams.alpha_mse_for = 0.25
         mod.hparams.n_grad = 5
         mod.hparams.k_n_grad = 2
     
