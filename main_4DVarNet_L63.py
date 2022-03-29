60a77b7e174db78499184060213d2824e75ead94
rfablet <ronan.fablet@imt-atlantique.fr>
2022-03-29 22:09:57 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1221,7 +1221,7 @@ class LitModel_4dvar_classic(pl.LightningModule):
                 x_for = self.phi.ode_int( x_curr[:,:,dT-dt_forecast-1] , dt_forecast )
                 
                 print( x_for.size() )                
-                outputs[:,:,dT-dt_forecast:] = x_for
+                outputs[:,:,dT-dt_forecast-1:] = x_for.view(-1,3,dt_forecast+1,1)
                     
                 
             if flag_x1_only == False:
