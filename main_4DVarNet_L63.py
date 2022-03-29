16215f918e3015cf807a947730a50869546d13c3
rfablet <ronan.fablet@imt-atlantique.fr>
2022-03-29 21:36:30 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1458,8 +1458,8 @@ if __name__ == '__main__':
         
         print(mod.hparams)
         mod.alpha_prior = 1e4
-        mod.alpha_obs = 5e4
-        mod.lam = 0.5
+        mod.alpha_obs = 1e5
+        mod.lam = 0.2
         mod.n_iter_descent = 20000
     
         #trainer = pl.Trainer(gpus=1, accelerator = "ddp", **profiler_kwargs)
