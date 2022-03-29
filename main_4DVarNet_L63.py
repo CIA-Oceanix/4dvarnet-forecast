8aeb4eaa4ff561d6774c68e3bc0fd329dc6fd3d3
rfablet <ronan.fablet@imt-atlantique.fr>
2022-03-29 09:22:20 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -28,7 +28,7 @@ from scipy.integrate import solve_ivp
 #from AnDA_codes.AnDA_dynamical_models import AnDA_Lorenz_63, AnDA_Lorenz_96
 from sklearn.feature_extraction import image
 
-flagProcess = 0
+flagProcess = 1
 
 dimGradSolver = 25
 rateDropout = 0.2
@@ -48,7 +48,7 @@ rateMissingData = (1-1./8.)#0.75#0.95
 flagTypeMissData = 2
 flagForecast = True#False#
 dt_forecast = 55
-flag_x1_only = True #False#
+flag_x1_only = False#True #
 
 print('........ Data generation')
 flagRandomSeed = 0
@@ -1148,6 +1148,8 @@ if __name__ == '__main__':
         
         pathCheckPOint = 'resL63/exp02-2/model-l63-forecast_055-aug10-unet2+wc_ode-exp02-2-Noise01-igrad05_02-dgrad25-drop20-epoch=111-val_loss=2.03.ckpt'
         pathCheckPOint = 'resL63/exp02-2/model-l63-forecast_050-aug10-unet2-exp02-2-Noise01-igrad05_02-dgrad25-drop20-epoch=127-val_loss=1.98.ckpt'
+        
+        pathCheckPOint = 'resL63/exp02-2/model-l63-forecast_055-aug10-unet2-exp02-2-Noise01-igrad05_02-dgrad25-drop20-epoch=73-val_loss=71.93.ckpt'
         
         print('.... load pre-trained model :'+pathCheckPOint)
         
