e987bea60cd9c2ec50b62e074cddac27f6355757
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-01 21:43:28 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -29,9 +29,9 @@ from scipy.integrate import solve_ivp
 #from AnDA_codes.AnDA_dynamical_models import AnDA_Lorenz_63, AnDA_Lorenz_96
 from sklearn.feature_extraction import image
 
-flagProcess = 0
+flagProcess = 1
 
-dimGradSolver = 50
+dimGradSolver = 25
 rateDropout = 0.2
 DimAE = 10
 flagAEType = 'unet2'#'unet'#'unet2+wc_ode'#'unet' # #'ode' # 
