b6e46c15909eb9f1df40de1be9b2ceab8addd093
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-01 17:32:17 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -29,7 +29,7 @@ from scipy.integrate import solve_ivp
 #from AnDA_codes.AnDA_dynamical_models import AnDA_Lorenz_63, AnDA_Lorenz_96
 from sklearn.feature_extraction import image
 
-flagProcess = 2
+flagProcess = 0
 
 dimGradSolver = 25
 rateDropout = 0.2
@@ -123,7 +123,7 @@ if flag_load_data == False :
         S0 = solve_ivp(fun=lambda t,y: AnDA_Lorenz_63(y,t,GD.parameters.sigma,GD.parameters.rho,GD.parameters.beta),t_span=[0.,nb_seq+5+0.000001],y0=y0,first_step=GD.dt_integration,t_eval=np.arange(0,nb_seq+5+0.000001,GD.dt_integration),method='RK45')
         
         print(S0.y.shape)
-        GD.nb_loop_seq = 10000
+        GD.nb_loop_seq = 20000
         tt = np.arange(GD.dt_integration,GD.nb_loop_seq*GD.dt_integration+0.000001,GD.dt_integration)
         
         for nn in range(0,10):
@@ -141,8 +141,8 @@ if flag_load_data == False :
             xt.values = S
             xt.time   = tt
             # extract subsequences
-            dataTrainingNoNaN_nn = image.extract_patches_2d(xt.values[0:7500:time_step,:],(dT,3),max_patches=int(NbTraining/nb_seq))
-            dataTestNoNaN_nn     = image.extract_patches_2d(xt.values[8000::time_step,:],(dT,3),max_patches=int(NbTest/nb_seq))
+            dataTrainingNoNaN_nn = image.extract_patches_2d(xt.values[0:12500:time_step,:],(dT,3),max_patches=int(NbTraining/nb_seq))
+            dataTestNoNaN_nn     = image.extract_patches_2d(xt.values[15000::time_step,:],(dT,3),max_patches=int(NbTest/nb_seq))
             
             if nn == 0 :
                 dataTrainingNoNaN = np.copy( dataTrainingNoNaN_nn )
