03b53e5bd71c96f1f43d8b1a76ec9e42fb1c9707
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-07 10:53:43 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1848,8 +1848,8 @@ if __name__ == '__main__':
         mod.hparams.alpha_mse = 1.
         mod.hparams.alpha_mse_rec = 0.75
         mod.hparams.alpha_mse_for = 0.25
-        mod.hparams.n_grad = 5
-        mod.hparams.k_n_grad = 3
+        mod.hparams.n_grad = 15
+        mod.hparams.k_n_grad = 1
     
         print(' Ngrad = %d / %d'%(mod.hparams.n_grad,mod.model.n_grad))
         #trainer = pl.Trainer(gpus=1, accelerator = "ddp", **profiler_kwargs)
