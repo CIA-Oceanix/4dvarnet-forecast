be66ef9c6e6f0d65f7766c9c054453d46c7c0995
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-07 12:34:53 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1713,7 +1713,7 @@ if __name__ == '__main__':
       
     if flagProcess == 0: ## training model from scratch
         
-        flagLoadModel = True# False #  
+        flagLoadModel = False #  True# 
         if flagLoadModel == True:
             
             pathCheckPOint = 'resL63/exp02-2/model-l63-forecast_055-aug10-unet2-exp02-2-Noise01-igrad05_02-dgrad25-drop20-epoch=105-val_loss=2.08.ckpt'
@@ -1741,8 +1741,8 @@ if __name__ == '__main__':
         else:
             mod = LitModel()
             
-            mod.hparams.n_grad          = 5#1#5
-            mod.hparams.k_n_grad        = 2
+            mod.hparams.n_grad          = 10#1#5
+            mod.hparams.k_n_grad        = 1
             mod.hparams.iter_update     = [0, 100, 200, 300, 500, 700, 800]  # [0,2,4,6,9,15]
             mod.hparams.nb_grad_update  = [5, 5, 10, 10, 15, 15, 20, 20, 20]  # [0,0,1,2,3,3]#[0,2,2,4,5,5]#
             mod.hparams.lr_update       = [1e-3, 1e-4, 1e-4, 1e-5, 1e-4, 1e-5, 1e-5, 1e-6, 1e-7]
@@ -1753,10 +1753,10 @@ if __name__ == '__main__':
         mod.hparams.alpha_mse = 1.
         mod.hparams.alpha_mse_rec = (dT-dt_forecast)/dT #0.75
         mod.hparams.alpha_mse_for = dt_forecast/dT #0.5#0.25
-        mod.hparams.alpha_4dvarloss_diff = 0.1 #0.#
+        mod.hparams.alpha_4dvarloss_diff = 0.#0.1 #
 
-        mod.hparams.noise_rnd_lstm_init = 1e-2 #0.
-        mod.hparams.noise_rnd_aug_init = 1e-2 #0.
+        mod.hparams.noise_rnd_lstm_init = 0.#1e-2 #0.
+        mod.hparams.noise_rnd_aug_init = 0.#1e-2 #0.
 
         mod.hparams.rate_rnd_init = 0. #0.25               
 
