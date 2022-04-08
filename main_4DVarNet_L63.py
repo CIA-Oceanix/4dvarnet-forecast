7fdb3e7d4a87242db7d0e5b5c3dab8ad3e0c04c0
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-08 17:59:48 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -448,7 +448,8 @@ else:
         x_test_obs = x_test_obs[:,:,::10]
         
         x_train_obs = 0. * x_train_obs
-        x_train_obs[:,0,::8] = x_train[:,0,::8] + 0.1 * np.random.randn(x_train.shape[0],1,32)
+        x_train_obs[:,0,::8] = x_train[:,0,::8] 
+        print(x_train_obs[:,0,::8].shape)
         mask_train = 0. * mask_train
         mask_train[:,:,::8] = 1.
 
