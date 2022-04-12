b7e290e5023521b069b78345c44280af784c5ac9
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-12 14:56:32 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1951,7 +1951,7 @@ if __name__ == '__main__':
         mod.hparams.alpha_mse_rec = (dT-dt_forecast)/dT #0.75
         mod.hparams.alpha_mse_for = dt_forecast/dT #0.5#0.25
         mod.hparams.n_grad = 5
-        mod.hparams.k_n_grad = 2
+        mod.hparams.k_n_grad = 1
 
         mod.flag_ode_forecast = False#True#
     
