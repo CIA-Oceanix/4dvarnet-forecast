7f1b99f53b5a3a5f6422225e7cbd6c29109bc2d5
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-12 09:29:10 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -413,8 +413,8 @@ else:
                         
     ncfile = Dataset(path_l63_dataset,"r")
     x_train = ncfile.variables['x_train'][:]
-    #x_train_Init = ncfile.variables['x_train_Init'][:]
-    x_train_Init = ncfile.variables['x_train_init'][:]
+    x_train_Init = ncfile.variables['x_train_Init'][:]
+    #x_train_Init = ncfile.variables['x_train_init'][:]
     x_train_obs = ncfile.variables['x_train_obs'][:]
     mask_train = ncfile.variables['mask_train'][:]
 
