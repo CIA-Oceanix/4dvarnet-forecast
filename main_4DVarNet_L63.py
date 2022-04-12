617b4e75051811a2d1199de32444526dd6d2f15f
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-12 14:43:50 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -450,13 +450,13 @@ else:
         
     if 1*1 :
         x_train_obs = 0. * x_train_obs
-        x_train_obs[:,0,::8] = x_train[:,0,::8] + 0.1 * np.random.randn(5000,32)
+        x_train_obs[:,0,::8] = x_train[:,0,::8] + 0. * np.random.randn(5000,32)
         mask_train = 0. * mask_train
         mask_train[:,0,::8] = 1.
         x_train_Init = 0. * x_train_obs
 
         x_test_obs = 0. * x_test_obs
-        x_test_obs[:,0,::8] = x_test[:,0,::8] + 0.1 * np.random.randn(100,32)
+        x_test_obs[:,0,::8] = x_test[:,0,::8] + 0.0 * np.random.randn(100,32)
         mask_test = 0. * mask_test
         mask_test[:,0,::8] = 1.
         x_test_Init = 0. * x_test_obs
