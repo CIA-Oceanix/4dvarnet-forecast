6e8fd3ce388f565bc49d0cff51baee36dbe1cb6e
rfablet <ronan.fablet@imt-atlantique.fr>
2022-04-30 23:44:11 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -31,7 +31,7 @@ from sklearn.feature_extraction import image
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-flagProcess = 1
+flagProcess = 0
 
 dimGradSolver = 25
 rateDropout = 0.2
@@ -1853,7 +1853,7 @@ if __name__ == '__main__':
             
             mod.hparams.n_grad          = 5#1#5
             mod.hparams.k_n_grad        = 2
-            mod.hparams.iter_update     = [0, 100, 200, 200, 300, 500, 700, 800]  # [0,2,4,6,9,15]
+            mod.hparams.iter_update     = [0, 300, 200, 200, 300, 500, 700, 800]  # [0,2,4,6,9,15]
             mod.hparams.nb_grad_update  = [5, 5, 10, 10, 15, 15, 20, 20, 20]  # [0,0,1,2,3,3]#[0,2,2,4,5,5]#
             mod.hparams.lr_update       = [1e-3, 1e-4, 1e-4, 1e-5, 1e-4, 1e-5, 1e-5, 1e-6, 1e-7]
         
@@ -1861,9 +1861,9 @@ if __name__ == '__main__':
         
         mod.hparams.alpha_prior = 0.1
         mod.hparams.alpha_mse = 1.
-        mod.hparams.alpha_mse_rec = 1. #(dT-dt_forecast)/dT #0.75
+        mod.hparams.alpha_mse_rec = 0.1 #(dT-dt_forecast)/dT #0.75
         mod.hparams.alpha_mse_for = 10. #dt_forecast/dT #0.5#0.25
-        mod.hparams.alpha_mse_init = 10. #0.75
+        mod.hparams.alpha_mse_init = 1. #0.75
 
         mod.hparams.alpha_4dvarloss_diff = 0.1#5.e1#0.1 #
 
@@ -1875,7 +1875,7 @@ if __name__ == '__main__':
         mod.x_rec_training = x_train_Init[:idx_val,:,:,:]
         mod.x_rec_val = x_train_Init[idx_val:,:,:,:]
         
-        profiler_kwargs = {'max_epochs': 400 }
+        profiler_kwargs = {'max_epochs': 300 }
 
         suffix_exp = 'exp%02d-new'%flagTypeMissData
         #suffix_exp = 'exp_perrine%02d-'%flagTypeMissData
