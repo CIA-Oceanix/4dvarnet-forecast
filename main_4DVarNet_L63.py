75ee9a1c484ce517c6c5e03cb1a9de104929e195
rfablet <ronan.fablet@imt-atlantique.fr>
2022-05-01 15:36:32 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -1973,7 +1973,7 @@ if __name__ == '__main__':
         pathCheckPOint = 'resL63/exp02-newdata/model-l63-forecast_055--rec001--for100--init010-unet2-exp02-newdata-Noise01-igrad05_02-dgrad25-drop20-epoch=161-val_loss=8.19.ckpt'
         pathCheckPOint = 'resL63/exp02-newdata/model-l63-forecast_055--rec001--for100--init010-unet2-exp02-newdata-Noise01-igrad05_03-dgrad25-drop20-epoch=106-val_loss=8.12.ckpt'
         
-        pathCheckPOint = 'resL63/exp02-newdata/model-l63-newforecast_055--rec001--for100--init010-unet2-exp02-newdata-Noise01-igrad05_02-dgrad25-drop20-epoch=18-val_loss=5.36.ckpt'
+        pathCheckPOint = 'resL63/exp02-newdata/model-l63-newforecast_055--rec001--for100--init010-unet2-exp02-newdata-Noise01-igrad05_02-dgrad25-drop20-epoch=21-val_loss=4.88.ckpt'
         
         print('.... load pre-trained model :'+pathCheckPOint)
         
