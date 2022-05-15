0976769338a8a76a2f61db25c19a3c699945a55d
rfablet <ronan.fablet@imt-atlantique.fr>
2022-05-15 23:14:55 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -37,7 +37,7 @@ flagProcess = 0
 dimGradSolver = 25
 rateDropout = 0.2
 DimAE = 10
-flagAEType = 'unet2'#ode'#'unet2'# 'ode'#'unet'#'unet2+wc_ode'#'unet' # #'ode' # 
+flagAEType = 'unet-1d-8'#'unet2'#ode'#'unet2'# 'ode'#'unet'#'unet2+wc_ode'#'unet' # #'ode' # 
 #flagAEType = 'unet-1d'
 dim_aug_state = 0#10#10#10#10 #False#
  
@@ -1020,7 +1020,93 @@ elif flagAEType == 'unet2+wc_ode': ## Conv model with no use of the central poin
           return xpred
  
 elif flagAEType == 'unet-1d': ## Conv model with no use of the central point
-  class Phi_r(torch.nn.Module):
+    class UNet_1D(nn.Module):
+        def __init__(self, n_channels, n_classes, bilinear=False):
+            super(UNet_1D, self).__init__()
+            self.n_channels = n_channels
+            self.n_classes = n_classes
+            self.bilinear = bilinear
+    
+            self.inc = unet.DoubleConv_1D(n_channels, 32)#,padding_mode=padding_mode,activation='relu')
+            self.down1 = unet.Down_1D(32, 64)
+            self.down2 = unet.Down_1D(64, 128)
+            #self.down3 = Down(256, 512)
+            factor = 2 if bilinear else 1
+            
+            #self.down4 = Down(512, 1024 // factor)
+            #self.up1 = Up(1024, 512 // factor, bilinear)
+            #self.up2 = Up(512, 256 // factor, bilinear)
+            self.up3 = unet.Up_1D(128, 64 // factor, bilinear)
+            self.up4 = unet.Up_1D(64, 32, bilinear)
+            self.outc = unet.OutConv_1D(32, n_classes)
+    
+        def forward(self, x):
+            x1 = self.inc(x)
+            x2 = self.down1(x1)
+            x3 = self.down2(x2)
+            #x4 = self.down3(x3)
+            #x5 = self.down4(x4)
+            #x = self.up1(x5, x4)
+            #x5 = self.down4(x4)
+            #x = self.up1(x5, x4)
+            #print(x3.shape)
+            #print(x4.shape)
+            #x = self.up2(x4, x3)
+            x = self.up3(x3, x2)
+            x = self.up4(x, x1)
+            out = self.outc(x)
+            
+            return out
+        
+    class Phi_r(torch.nn.Module):
+      def __init__(self):
+          super(Phi_r, self).__init__()
+          self.unet  = unet.UNet_1D(3,3,False)
+          
+      def forward(self, xinp):
+          xout = self.unet( xinp.view(-1,xinp.size(1),xinp.size(2)) )
+          return xout.view(-1,xinp.size(1),xinp.size(2),1)
+
+elif flagAEType == 'unet-1d-8': ## Conv model with no use of the central point
+    class UNet_1D(nn.Module):
+        def __init__(self, n_channels, n_classes, bilinear=False):
+            super(UNet_1D, self).__init__()
+            self.n_channels = n_channels
+            self.n_classes = n_classes
+            self.bilinear = bilinear
+    
+            self.inc = unet.DoubleConv_1D(n_channels, 8)#,padding_mode=padding_mode,activation='relu')
+            self.down1 = unet.Down_1D(8, 16)
+            self.down2 = unet.Down_1D(16, 32)
+            self.down3 = unet.Down_1D(32, 64)
+            factor = 2 if bilinear else 1
+            
+            #self.down4 = Down(512, 1024 // factor)
+            #self.up1 = Up(1024, 512 // factor, bilinear)
+            self.up2 = unet.Up_1D(64, 32 // factor, bilinear)
+            self.up3 = unet.Up_1D(32, 16 // factor, bilinear)
+            self.up4 = unet.Up_1D(16, 8, bilinear)
+            self.outc = unet.OutConv_1D(8, n_classes)
+    
+        def forward(self, x):
+            x1 = self.inc(x)
+            x2 = self.down1(x1)
+            x3 = self.down2(x2)
+            x4 = self.down3(x3)
+            #x5 = self.down4(x4)
+            #x = self.up1(x5, x4)
+            #x5 = self.down4(x4)
+            #x = self.up1(x5, x4)
+            #print(x3.shape)
+            #print(x4.shape)
+            x = self.up2(x4, x3)
+            x = self.up3(x, x2)
+            x = self.up4(x, x1)
+            out = self.outc(x)
+            
+            return out
+        
+    class Phi_r(torch.nn.Module):
       def __init__(self):
           super(Phi_r, self).__init__()
           self.unet  = unet.UNet_1D(3,3,False)
