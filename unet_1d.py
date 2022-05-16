e704c731edee6a8f690fcd59af2d927579a8098b
rfablet <ronan.fablet@imt-atlantique.fr>
2022-05-16 09:29:59 +0200

    xx

--- unet_1d.py
+++ unet_1d.py
@@ -142,47 +142,6 @@ class DoubleConvBILIN(nn.Module):
         
         return x1
 
-class Down2(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels,mid_channels=None,padding_mode='reflect',activation='relu'):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            #nn.AvgPool2d(2),
-            DoubleConv(in_channels, out_channels, mid_channels,padding_mode=padding_mode,activation='relu')
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-class Down2Avg(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels,mid_channels=None,padding_mode='reflect',activation='relu'):
-        super().__init__()
-        self.avgpool_conv = nn.Sequential(
-            #nn.MaxPool2d(2),
-            nn.AvgPool2d(2),
-            DoubleConv(in_channels, out_channels, mid_channels,padding_mode=padding_mode,activation='relu')
-        )
-
-    def forward(self, x):
-        return self.avgpool_conv(x)
-
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            #nn.AvgPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
 
 class Down_1D(nn.Module):
     """Downscaling with maxpool then double conv"""
@@ -198,46 +157,6 @@ class Down_1D(nn.Module):
     def forward(self, x):
         return self.maxpool_conv(x)
 
-class Down4(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(4),
-            DoubleConv(in_channels, out_channels)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
 
 class Up_1D(nn.Module):
     """Upscaling then double conv"""
@@ -261,6 +180,8 @@ class Up_1D(nn.Module):
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
+        
+        
         
         return self.conv(x)
 
@@ -286,6 +207,9 @@ class Up_1D_2(nn.Module):
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
+        
+        print(x2.size())
+        print(x1.size())
         
         return self.conv(x)
 
