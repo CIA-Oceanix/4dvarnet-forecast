c52597d22cd706a0e4f0f751ce25e4cd070be09e
rfablet <ronan.fablet@imt-atlantique.fr>
2022-05-16 08:53:43 +0200

    xx

--- main_4DVarNet_L63.py
+++ main_4DVarNet_L63.py
@@ -37,7 +37,7 @@ flagProcess = 3
 dimGradSolver = 25
 rateDropout = 0.2
 DimAE = 32#10
-flagAEType = 'unet-1d-bilin'#unet2'#'unet-1d-tanh'#'unet-1d-relu'#'#ode'#'unet2'# 'ode'#'unet'#'unet2+wc_ode'#'unet' # #'ode' # 
+flagAEType = 'unet2'#'unet-1d-bilin'#unet2'#'unet-1d-tanh'#'unet-1d-relu'#'#ode'#' 'ode'#'unet'#'unet2+wc_ode'#'unet' # #'ode' # 
 #flagAEType = 'unet-1d'
 dim_aug_state = 0#10#10#10#10 #False#
  
